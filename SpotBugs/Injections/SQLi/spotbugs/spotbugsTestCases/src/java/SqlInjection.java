bc6e8e4bf592b1c36b74aa26f092e1a150195c99
Magnus Karlsen <>
2019-05-30 10:20:09 +0200

    SpotBugs changes for SQL injection

---
+++ SpotBugs/Injections/SQLi/spotbugs/spotbugsTestCases/src/java/SqlInjection.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import edu.umd.cs.findbugs.annotations.Confidence;
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+class SqlInjection {
+}
